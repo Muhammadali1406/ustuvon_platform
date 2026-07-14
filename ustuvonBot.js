1f0fc7dd842687e21f157b6796ce4e6eea5c8dfe
Muhammadali1406 <rmuhammadali316@gmail.com>
2026-07-14 16:37:05 +0500

    men edim

--- ustuvonBot.js
+++ ustuvonBot.js
@@ -13,7 +13,72 @@ const users = [
     {id: 8396840695,name: "Janob boshliq",profession: "fullstack", type: "offline"},
     {id: 8251893506,name: "Janob boshbori",profession: "fullstack", type: "online"},
     //{ id: 6917684084, name: "Qodirov Biloliddin", profession: "backend" },
-    
+    {
+        id: 1417999336,
+        name: "Muslimbek Bostonov",
+        profession: "frontend",
+        type: "offline"
+    },
+    {
+        id: 6461443178,
+        name: "Davronbek Nazarov",
+        profession: "backend",
+        type: "offline"
+    },
+    {
+        id: 5259707725,
+        name: "Abdulaziz Komiljonov",
+        profession: "backend",
+        type: "online"
+    },
+    {
+        id: 8270492933,
+        name: "Yuldoshev Sirojiddin",
+        profession: "backend",
+        type: "online"
+    },
+    {
+        id: 5817905119,
+        name: "Samandar Qodirov",
+        profession: "frontend",
+        type: "online"
+    },
+    {
+        id: 6690920445,
+        name: "Muhammadjon Mashrabov",
+        profession: "frontend",
+        type: "online"
+    },
+    {
+        id: 6134458285,
+        name: "Davronbek Nabijonov",
+        profession: "frontend",
+        type: "offline"
+    },
+    {
+        id: 1306147337,
+        name: "Farrux To'g'onov",
+        profession: "fullstack",
+        type: "offline"
+    },
+    {
+        id: 1771891844,
+        name: "Oqilbek Erkinov",
+        profession: "fullstack",
+        type: "offline"
+    },
+    {
+        id: 6466116037,
+        name: "Dilshod Mahmudov",
+        profession: "fullstack",
+        type: "online"
+    },
+    {
+        id: 2035940312,
+        name: "Fozilov Muzaffar",
+        profession: "designer",
+        type: "offline"
+    }
 ];
 
 // Admin tekshirish
@@ -90,8 +155,8 @@ Avvalo, sizni jamoamizga qo'shilganingiz bilan tabriklayman. Ishonamanki, birgal
 
 Sizlar online formatda faoliyat yuritasizlar. Yaqin kunlarda zoom yoki watsap orqali online meeting tashkil etib har biringizga loyiha tafsilotlari, ish tartibi, vazifalar va foydalaniladigan platformalar haqida batafsil ma'lumot beriladi.
 
-Har biringizga omad, muvaffaqiyat va unumli ish faoliyatini tilayman. Birgalikda kuchli jamoa bo'lib rivojlanamiz! 🚀
-`
+Har biringizga omad, muvaffaqiyat va unumli ish faoliyatini tilayman. Birgalikda kuchli jamoa bo'lib rivojlanamiz! 🚀\n\nAgar siz Farg'ona viloyatida offline ishlay olaman desangiz @muhammadali_1406 ga murojat qilishingizni so'raymiz`
+
 
 
             const message = user.type === "online" ? message2 : message1;
